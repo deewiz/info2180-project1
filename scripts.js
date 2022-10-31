513275b259b45dc8cea0ada248e30748dffb7e70
Darrell Morgan <darrell.morgan02.dm@gmail.com>
2022-10-31 11:03:10 -0500

    final tweaks

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 /* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function(){
+    let msg = document.getElementsByClassName("message")[0];
+    let btn = document.getElementsByTagName("button")[0];
+    let email = document.getElementById("email");
+    
+  
+    btn.addEventListener('click', (event) =>{
+        event.preventDefault();
+        if (email.value === '' || email.value === null){
+            msg.textContent = 'Please enter a valid email address.';
+            //console.log("200");
+        }
+        else{
+            msg.textContent = `Thank you! Your email address, ${email.value}, has been added to our mailing list!`;
+            //console.log("200");
+         }
+    });
+     console.log("Okay. JS loaded successfully.")
+ }, false);
